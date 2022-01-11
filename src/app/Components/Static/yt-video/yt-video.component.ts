db04eb98792a8d3287f5b2feee42a4b1b05b7f21
Diana Ponce <dianagpr17@gmail.com>
2022-01-11 16:04:39 -0600

    [VideoYouTube] - Se modifica componente en donde primero se muestra una imagen, después de hacer click se carga el iframe que coloca el video de youtube.

--- src/app/Components/Static/yt-video/yt-video.component.ts
+++ src/app/Components/Static/yt-video/yt-video.component.ts
@@ -4,15 +4,22 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   selector: 'app-yt-video',
   templateUrl: './yt-video.component.html',
   styleUrls: ['./yt-video.component.css'],
-  encapsulation: ViewEncapsulation.ShadowDom 
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class YtVideoComponent implements OnInit {
   public reproductor = true;
+  public show_iframe = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
   reproducir(){
     this.reproductor = true;
+  }
+
+  showVideo($event) {
+    $event.target.style.display = 'none'
+    this.show_iframe = true
   }
 }
